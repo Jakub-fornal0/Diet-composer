c3e8dcdeeff3215c58b4b5ca2844356a5e9044b0
Jakub Fornal <jakub.fornal0@onet.pl>
2022-08-04 15:29:37 +0200

    register form

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { registerData } from '../../interfaces/register.model';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
+  registerForm: FormGroup;
+  passwordIsHide: boolean = true;
+  registerDataAreCorrect: boolean = true;
+  registerData: registerData = { userName: '', email: '', password: '' };
+  error: String = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
   }
 
+  ngOnInit(): void {}
+
+  register() {
+    if (!this.registerForm.valid) {
+      this.registerForm.markAllAsTouched();
+    }
+  }
 }
---
+++ frontend/src/app/interfaces/register.model.ts
@@ -0,0 +1,5 @@
+export interface registerData {
+  userName: String;
+  email: String;
+  password: String;
+}
