aff412ed44fb7880c78cfa01103b9b55d0f4bd46
Jakub Fornal <jakub.fornal0@onet.pl>
2022-08-13 12:42:52 +0200

    add recipes component

--- frontend/src/app/components/recipes/recipes.component.ts
+++ frontend/src/app/components/recipes/recipes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Product } from './../../interfaces/product.model';
+import { Recipe } from './../../interfaces/recipe.model';
 import { LocalStorageService } from '../../services/local-storage.service';
 import { LocalStorageConsts } from '../../consts/localstorage-consts';
 import { MatDialog } from '@angular/material/dialog';
@@ -13,6 +14,119 @@ import { RecipesFilterDialogComponent } from './recipes-filter-dialog/recipes-fi
 })
 export class RecipesComponent implements OnInit {
   products: Product[] = [];
+
+  // MOCKUP USUNAC POTEM //
+  recipes: Recipe[] = [
+    {
+      id: 1,
+      image: 'assets/zdj.jpg',
+      name: '1 Przepis Przepis Przepis',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      cookingTime: '45min',
+      portions: 4,
+    },
+    {
+      id: 2,
+      image: 'assets/zdj.jpg',
+      name: '2 Przepis Przepis Przepis',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      cookingTime: '45min',
+      portions: 4,
+    },
+    {
+      id: 3,
+      image: 'assets/zdj.jpg',
+      name: '3 Przepis Przepis Przepis',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      cookingTime: '45min',
+      portions: 4,
+    },
+    {
+      id: 4,
+      image: 'assets/zdj.jpg',
+      name: '4 Przepis Przepis Przepis',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      cookingTime: '45min',
+      portions: 4,
+    },
+    {
+      id: 5,
+      image: 'assets/zdj.jpg',
+      name: '5 Przepis Przepis Przepis',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      cookingTime: '45min',
+      portions: 4,
+    },
+    {
+      id: 6,
+      image: 'assets/zdj.jpg',
+      name: '6 Przepis Przepis Przepis',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      cookingTime: '45min',
+      portions: 4,
+    },
+    {
+      id: 7,
+      image: 'assets/zdj.jpg',
+      name: '7 Przepis Przepis Przepis',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      cookingTime: '45min',
+      portions: 4,
+    },
+    {
+      id: 8,
+      image: 'assets/zdj.jpg',
+      name: '8 Przepis Przepis Przepis',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      cookingTime: '45min',
+      portions: 4,
+    },
+    {
+      id: 9,
+      image: 'assets/zdj.jpg',
+      name: '9 Przepis Przepis Przepis',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      cookingTime: '45min',
+      portions: 4,
+    },
+    {
+      id: 10,
+      image: 'assets/zdj.jpg',
+      name: '10 Przepis Przepis Przepis',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      cookingTime: '45min',
+      portions: 4,
+    },
+    {
+      id: 11,
+      image: 'assets/zdj.jpg',
+      name: '11 Przepis Przepis Przepis',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      cookingTime: '45min',
+      portions: 4444,
+    },
+    {
+      id: 12,
+      image: 'assets/zdj.jpg',
+      name: '12 Przepis Przepis Przepis',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      cookingTime: '45min',
+      portions: 111,
+    },
+  ];
+  // ___________________ //
 
   @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
 
@@ -65,6 +179,6 @@ export class RecipesComponent implements OnInit {
   }
 
   onChangedPage(pageData: PageEvent) {
-    console.log('dupa');
+    console.log('');
   }
 }
---
+++ frontend/src/app/interfaces/recipe.model.ts
@@ -0,0 +1,8 @@
+export interface Recipe {
+  id?: number;
+  image: string;
+  name: string;
+  description: string;
+  cookingTime: string;
+  portions: number;
+}
