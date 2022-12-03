a18da08b564cee64326f330f4b154497ca99f9ae
Adrian Filipiuk <adrianfilip27@gmail.com>
2022-12-03 20:47:23 +0100

    Added testing homeController

--- backend/controllers/scheduleController.js
+++ backend/controllers/scheduleController.js
@@ -24,7 +24,7 @@ exports.wholeSchedule = async (req, res) => {
       include: [
         {
           model: recipes,
-          attributes: [],
+          attributes: []
         },
       ],
       where: {
@@ -33,6 +33,7 @@ exports.wholeSchedule = async (req, res) => {
       order: [["temp", "ASC"]],
       raw: true,
     }).then(async (meals) => {
+      JSON.stringify(meals);
         let 
             breakfast = {}, 
             secondBreakfast = {}, 
@@ -60,11 +61,12 @@ exports.wholeSchedule = async (req, res) => {
             array.recipeImage="http://localhost:3000/imagesRecipe/" + array.recipeImage + ".png";
           });
           let snacks = await snack.findAll({
-            attributes:["snackName", "calories", "fats", "proteins", "carbohydrates"],
+            attributes:["id", "snackName", "calories", "fats", "proteins", "carbohydrates"],
             where:{
                 userId: userId,
             }
-          })
+          });
+          
         res.status(200).send({ breakfast, secondBreakfast, lunch, tea, dinner, snacks });
       }).catch((err) => {
         res.status(402).send({
@@ -114,15 +116,9 @@ exports.deleteSchedule = async (req, res) => {
 exports.addToSchedule = async (req, res) => {
   try {
     const userId = jwt.decode(req.headers["x-access-token"]).id;
-    const { recipeId, time } = req.body;
+    const { recipeId, time, type } = req.body;
     let temp;
-    let type = await recipes.findOne({
-      attributes: ["category"],
-      where: {
-        id: recipeId,
-      },
-    });
-    type = type.category;
+    
     switch (type) {
       case "śniadanie":
         temp = 1;
@@ -267,7 +263,7 @@ exports.eaten = async (req, res) => {
       .then(async () => {
         await mealData
           .update(
-            { eaten: 1 },
+            { eaten: true },
             {
               where: {
                 recipeId: recipeId,
